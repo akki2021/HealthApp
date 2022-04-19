d3ec7f5f1985144e5e0f070dd120928ef6944ed9
ishir <ishiragrawal02@gmail.com>
2022-04-20 01:39:50 +0530

    profile and splash screen done

--- app/src/main/java/com/example/healthapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/healthapp/ui/profile/ProfileFragment.kt
@@ -4,62 +4,50 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.healthapp.databinding.FragmentProfileBinding
+import com.example.gdsc_hackathon.extensions.timer
+import com.example.healthapp.R
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.*
-import kotlinx.android.synthetic.main.fragment_profile.*
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_reply.*
 
-class ProfileFragment : Fragment() {
 
-    private var _binding: FragmentProfileBinding? = null
-    private lateinit var database: DatabaseReference
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var  name: TextView
+    private lateinit var  email: TextView
+    private lateinit var  age: TextView
+    private lateinit var  progressBar: ProgressBar
+
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this)[ProfileViewModel::class.java]
-
-        _binding = FragmentProfileBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-        val user=FirebaseAuth.getInstance().currentUser
-//        database=FirebaseDatabase.getInstance().getReference("User")
-//        if(user!=null){
-//            database.child(user.uid).addValueEventListener(object : ValueEventListener{
-//                override fun onDataChange(snapshot: DataSnapshot) {
-//
-//                    val map = snapshot.value as Map<String,String>?
-//                    val f_name=map!!["First_name"]
-//                    val l_name=map!!["Last_name"]
-//                    val emailm=map!!["Email"]
-//                    val Mobile=map!!["Mobile"]
-//                    val nam_e=f_name +" "+ l_name
-//                    name.text = nam_e
-//                    emaill.text=emailm
-//                    contact.text = Mobile
-//                }
-//
-//                override fun onCancelled(error: DatabaseError) {
-//
-//                }
-//            })
-//        }
+        val rootView: View = inflater.inflate(R.layout.fragment_profile, container, false)
+        name = rootView.findViewById(R.id.name)
+        email = rootView.findViewById(R.id.emaill)
+        age = rootView.findViewById(R.id.age)
+        progressBar = rootView.findViewById(R.id.progress_bar)
 
 
-        return root
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+
+        val user = FirebaseAuth.getInstance().currentUser
+        timer(progressBar, 400)
+        Firebase.firestore.collection("users").document(user!!.uid).get().addOnCompleteListener {
+            val doc = it.result
+            if(doc!=null) {
+                name.text = doc.getString("name").toString()
+                email.text = doc.getString("email").toString()
+                age.text = doc.getLong("age").toString()
+
+
+            }
+        }
+        return rootView
     }
 }
